c8810b596ae18ff1ddd0562b458bc7a04845d1e3
Nicky Knights <Nicky@MacBook-Air-de-Nicky.local>
2018-02-27 16:07:56 +0100

    He añadido el main.go

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	edadnicky, edadruben := 29, 38
+	fmt.Println("Hola Ru,", "\n", "Ahora son las", time.Now(), "\n", "Tienes", edadruben, "años y yo", edadnicky, ". Si los sumamos da", edadruben+edadnicky)
+}
