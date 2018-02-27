091dadd9032f5dddfc181c36e351edff1e95495f
Nicky Fusté <nicky.fuste@gmail.com>
2018-02-27 19:34:34 +0100

    Reescrito a Hello World

--- main.go
+++ main.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
-	edadnicky, edadruben := 29, 38
-	fmt.Println("Hola Ru,", "\n", "Ahora son las", time.Now(), "\n", "Tienes", edadruben, "años y yo", edadnicky, ". Si los sumamos da", edadruben+edadnicky)
+	fmt.Println("Hello World")
 }
