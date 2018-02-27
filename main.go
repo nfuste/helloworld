d681874c8e76a63ccaa1e5603b8273414d993bb2
Nicky Fusté <nicky.fuste@gmail.com>
2018-02-27 19:31:21 +0100

    Fuera mensaje de Ru

--- main.go
+++ main.go
@@ -8,5 +8,4 @@ import (
 func main() {
 	edadnicky, edadruben := 29, 38
 	fmt.Println("Hola Ru,", "\n", "Ahora son las", time.Now(), "\n", "Tienes", edadruben, "años y yo", edadnicky, ". Si los sumamos da", edadruben+edadnicky)
-	fmt.Println("Rubén motherfucker")
 }
