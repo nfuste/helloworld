9f010bb9494784a0fffe0e4e7ee7a228ac92fd63
Nicky Fusté <nicky.fuste@gmail.com>
2018-03-04 12:20:42 +0100

    Definido funciones diferentes

--- main.go
+++ main.go
@@ -2,11 +2,26 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"time"
 )
 
-func main() {
+func sayhello() {
+	fmt.Println("Hello World!")
+}
+
+func saytime() {
 	t := time.Now()
-	fmt.Println("Hello World", "The time is", t.Format("3:04PM"))
+	fmt.Println("The time is", t.Format("3:04PM"))
+}
+
+func saysqrt() {
+	fmt.Println("The square root of 4 is", math.Sqrt(4))
+}
+
+func main() {
+	sayhello()
+	saytime()
+	saysqrt()
 
 }
