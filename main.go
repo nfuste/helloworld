100fd3b041253cd27fc6ea345b33d5c9938db5cb
Nicky Fusté <nicky.fuste@gmail.com>
2018-03-04 12:42:07 +0100

    Añadido variable z en función saysqrt

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math"
+	"math/rand"
 	"time"
 )
 
@@ -16,12 +17,17 @@ func saytime() {
 }
 
 func saysqrt() {
-	fmt.Println("The square root of 4 is", math.Sqrt(4))
+	z := 4.0
+	fmt.Println("The square root of 4 is", math.Sqrt(z))
+}
+
+func sayrandomnumber() {
+	fmt.Println("A number from 1 to 100:", rand.Intn(100))
 }
 
 func main() {
 	sayhello()
 	saytime()
 	saysqrt()
-
+	sayrandomnumber()
 }
