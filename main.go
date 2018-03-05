d78bf261c4e1357fabf0bc658cea4f529ed543e9
Nicky Fusté <nicky.fuste@gmail.com>
2018-03-05 14:12:44 +0100

    More tests

--- main.go
+++ main.go
@@ -1,14 +1,13 @@
 package main
 
-import (
-	"fmt"
-	"math"
-	"math/rand"
-	"net/http"
-	"time"
-)
+import "fmt"
 
-func sayhello() {
+//"math"
+//"math/rand"
+//"net/http"
+//"time"
+
+/*func sayhello() {
 	fmt.Println("Hello World!") // Saludo inicial
 }
 
@@ -41,15 +40,49 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 
 func aboutHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Created by Nicky Knights")
+}*/
+
+type campaign struct {
+	permalink    uint32 // min 0 - max 2147483647
+	impressions  uint32
+	requests     uint32
+	campaignName string
+}
+
+func fillRate(x, y uint32) uint32 {
+	return x / y * 100
+}
+
+func fillRate2(c *campaign) uint32 {
+	fmt.Println(float64(c.impressions) / float64(c.requests))
+	return c.impressions / c.requests * 100
+}
+
+func (c *campaign) fillRate3() uint32 {
+	return c.impressions / c.requests * 100
 }
 
 func main() {
-	sayhello()
+	test()
+	/*sayhello()
 	saytime()
 	saysqrt()
 	sayrandomnumber()
 	saysum()
 	http.HandleFunc("/", indexHandler)
 	http.HandleFunc("/about", aboutHandler)
-	http.ListenAndServe(":8000", nil)
+	http.ListenAndServe(":8000", nil)*/
+	aCampaign := campaign{
+		permalink:    12345678,
+		impressions:  1234212,
+		requests:     2468424,
+		campaignName: "ADman Media Campaign"}
+
+	fmt.Println(fillRate(aCampaign.impressions, aCampaign.requests))
+
+	fmt.Println(fillRate2(&aCampaign)) // función - No se relaciona con la estructura, la trata como un parámetro
+
+	fmt.Println(aCampaign.fillRate3()) // método - Se relaciona con la estructura
+
+	fmt.Println("La campaña", aCampaign.campaignName, "tiene un fill rate del", aCampaign.fillRate3())
 }
